b2261d8ab7cc6511a2fdd915264968cbcd98ec55
Karol <Sliwa>
2019-01-29 14:52:34 +0100

    project 4 task 1

---
+++ Projekt 4/Task 1/project4_task1.js
@@ -0,0 +1,16 @@
+console.log('ok')
+
+document.body.addEventListener('mousemove', function (e) {
+    console.log(e.clientX, e.clientY);
+    var h1 = document.querySelector('h1');
+    // var clientX = e.clientX.toString()
+    // var clientY = e.clientY.toString()
+
+    //from the beginning on the actual viewed page
+    // h1.textContent = clientX + ', ' + clientY
+    // h1.textContent = e.clientX + ', ' + e.clientY;
+
+    //from the beginning of the page
+    h1.textContent = e.pageX + ', ' + e.pageY;
+
+});
