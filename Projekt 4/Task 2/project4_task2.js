df4fb066c3475c7393a2246147df31ccc4989b10
Karol <Sliwa>
2019-01-31 23:06:13 +0100

    project 4 task 2

--- Projekt 4/Task 2/project4_task2.js
+++ Projekt 4/Task 2/project4_task2.js
@@ -4,18 +4,41 @@ var body = document.querySelector('body');
 
 // add event listener on click
 
-body.addEventListener('click', function (e) {
-    console.log(e.clientY + ' ' + e.clientX)
-    var x = e.clientX;
-    var y = e.clientY;
-    if ((x % 2 === 0) && (y % 2 === 0)) {
-        console.log('obie parzyste');
-        body.style.backgroundColor = 'red';
-    } else if ((x % 2 !== 0) && (y % 2 !== 0)) {
-        console.log('obie nieparzyste');
-        body.style.backgroundColor = 'blue';
-    } else if ((x % 2 === 0) || (y % 2 == 0) && (x % 2 !== 0) || (y % 2 !== 0)) {
-        console.log('jedna nieparzysta druga parzysta');
-        body.style.backgroundColor = 'green';
-    }
+body.addEventListener('click', function (event) {
+    console.log(event.clientY + ' ' + event.clientX)
+    // var x = e.clientX;
+    // var y = e.clientY;
+    // if ((x % 2 === 0) && (y % 2 === 0)) {
+    //     console.log('obie parzyste');
+    //     body.style.backgroundColor = 'red';
+    // } else if ((x % 2 !== 0) && (y % 2 !== 0)) {
+    //     console.log('obie nieparzyste');
+    //     body.style.backgroundColor = 'blue';
+    // } else if ((x % 2 === 0) || (y % 2 == 0) && (x % 2 !== 0) || (y % 2 !== 0)) {
+    //     console.log('jedna nieparzysta druga parzysta');
+    //     body.style.backgroundColor = 'green';
+    // }
+
+    var color = getColor(event);
+    console.log(color);
+    document.body.style.backgroundColor = color;
+
 });
+
+var getColor = function (test) {
+
+    if (test.clientX % 2 === 0) {
+
+        if (test.clientY % 2 === 0) {
+            return 'red';
+        } else {
+            return 'green';
+        }
+    } else {
+        if (test.clientY % 2 === 0) {
+            return 'green'
+        }
+        return 'blue';
+    }
+
+};
