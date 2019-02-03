8426bbae8315ecfd56dc1806c8e0e399dcf4f340
Karol <Sliwa>
2019-02-03 13:20:03 +0100

    paoject 4 task 3 - swich metchod improved

--- Projekt 4/Task 4/project4_task4.js
+++ Projekt 4/Task 4/project4_task4.js
@@ -8,30 +8,35 @@ document.body.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue 
 console.log(document.body.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')');
 var changeColor = function (e) {
 
-    // IF 
-    // if (e.keyCode === 40) {
-    //     red -= 2
-    //     green -= 2
-    //     blue -= 2
-    // } else if (e.keyCode === 38) {
-    //     red += 2
-    //     green += 2
-    //     blue += 2
-    // }
-
-    // switch 
-    switch (e.keyCode) {
-        case 40:
+    // IF
+    if (e.keyCode === 40) {
+        if (red >= 0) {
             red -= 2
             green -= 2
             blue -= 2
-            break;
+        }
 
-        case 38:
+    } else if (e.keyCode === 38) {
+        if (red < 255) {
             red += 2
             green += 2
             blue += 2
+        }
+
+    }
+
+    //  switch
+    switch (e.keyCode) {
+        case 40:
+            red > 0 ? red -= 2 : red
+            green > 0 ? green -= 2 : green
+            blue > 0 ? blue -= 2 : blue;
             break;
+
+        case 38:
+            red < 255 ? red += 2 : red
+            green < 255 ? green += 2 : green
+            blue < 255 ? blue += 2 : blue;
     }
 
     console.log(e);
